327678eb82c8a925a33d25ebf5d769a654afb6a7
Vitali Zinkevich <vitalizinkevich@gmail.com>
2020-01-28 21:21:59 +0300

    фильтр отелей новый

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,10 +10,10 @@ import { SearchFormService } from './../services/search-form.service';
 })
 export class HomePage implements OnInit, OnDestroy {
     public hotels: any = [];
-    public filtredHotels;
+    public filtredHotels: any = [];
     public searchForm;
-    sub1;
-    sub2;
+    public sub1;
+    public sub2;
     constructor(
         private searchFormService: SearchFormService,
         private hotelsService: HotelsService,
@@ -28,52 +28,57 @@ export class HomePage implements OnInit, OnDestroy {
         this.searchForm = this.searchFormService.getSearchForm();
         this.sub1 = this.hotelsService.api.subscribe(data => {
             this.hotels = data;
-            this.filtredHotels = data;
+            this.filtredHotels = this.filterHotelList(
+                this.hotels,
+                this.searchForm.value
+            );
         });
 
         this.sub2 = this.searchForm.valueChanges.subscribe(form => {
-            this.filtredHotels =
-                this.hotels.length > 0
-                    ? this.hotels.filter(hotel => {
-                          if (form.stars && form.stars.length) {
-                              if (form.stars.indexOf(hotel.stars + '') > -1) {
-                                  return true;
-                              } else {
-                                  return false;
-                              }
-                          }
-                          if (form.location) {
-                              if (form.location && form.location.length) {
-                                  if (
-                                      form.location.indexOf(
-                                          hotel.location + ''
-                                      ) > -1
-                                  ) {
-                                      return true;
-                                  } else {
-                                      return false;
-                                  }
-                              }
-                          }
-                          console.log(form.searchString);
-                          if (form.searchString) {
-                              if (
-                                  hotel.name
-                                      .toUpperCase()
-                                      .indexOf(
-                                          form.searchString.toUpperCase()
-                                      ) > -1
-                              ) {
-                                  return true;
-                              } else {
-                                  return false;
-                              }
-                          }
-                          return true;
-                      })
-                    : this.hotels;
+            this.filtredHotels = this.filterHotelList(this.hotels, form);
         });
     }
+
+    public filterHotelList(list, form) {
+        list = list.filter(hotel => {
+            if (form.stars && form.stars.length) {
+                if (form.stars.indexOf(hotel.stars + '') > -1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        });
+        list = list.filter(hotel => {
+            if (form.location) {
+                if (form.location && form.location.length) {
+                    if (form.location.indexOf(hotel.location + '') > -1) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        });
+        list = list.filter(hotel => {
+            if (form.searchString) {
+                if (
+                    hotel.name
+                        .toUpperCase()
+                        .indexOf(form.searchString.toUpperCase()) > -1
+                ) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        });
+        return list;
+    }
+
     public priceShow() {
         this.searchForm.value.filtredHotelsForm = this.filtredHotels;
         this.router.navigate(['/list']);
